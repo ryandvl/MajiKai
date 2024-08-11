43527eedc9962cd3d28f324cf74207d20d4279f0
Ryan <126920703+ryandvl@users.noreply.github.com>
2024-08-11 13:47:33 -0300

    first commit

---
+++ src/main/java/me/ryandvl/majikai/MajiKai.java
@@ -0,0 +1,12 @@
+package me.ryandvl.majikai;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MajiKai implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
---
+++ src/main/java/me/ryandvl/majikai/client/MajiKaiClient.java
@@ -0,0 +1,12 @@
+package me.ryandvl.majikai.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MajiKaiClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    
+    }
+
+}
